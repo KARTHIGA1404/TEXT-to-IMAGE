2ce53848f0e2acad08f1257ee271566f0b6c0074
KARTHIGA1404 <1445karthiga@gmail.com>
2025-06-24 11:50:59 +0530

    initial commit

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='flex items-center justify-between gap-4 py-3 mt-20'>
+      <img src={assets.logo} alt="" width={150}/>
+      <p className='flex-1 border-l border-gray-400 pl-4 text-sm text-gray-500 max-sm:hidden'>Copyright @Karthiga | All right reserved.</p>
+      
+      <div className='flex gap-2.5 '>
+        <img src={assets.facebook_icon} alt="" width={35}/>
+        <a
+        href="https://x.com/karthiga1404?t=dfaYL6-LnLg0r-UkQ68IHw&s=09"
+        target="_blank"
+        rel="noopener noreferrer">
+        <img src={assets.twitter_icon} alt="Visit my profile" width={35} />
+        </a>
+
+        <a
+        href="https://www.instagram.com/karthiga_1404_?igsh=NTd2NGRsMDV1MjZy"
+        target="_blank"
+        rel="noopener noreferrer">
+        <img src={assets.instagram_icon} alt="Visit my profile" width={35} />
+        </a>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Footer
