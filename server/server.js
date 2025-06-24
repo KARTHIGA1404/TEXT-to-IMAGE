80f67275fb78d1f88e57433ed5cf7a6588838180
KARTHIGA1404 <1445karthiga@gmail.com>
2025-06-24 20:05:39 +0530

    backend hi

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app=express()
 
 app.use(express.json())
 app.use(cors({
-  origin: ["https://text-to-image-5fhz.vercel.app"],
+  origin: ["http://localhost:5173", "https://text-to-image-5fhz.vercel.app"],
   credentials: true
 }));
 await connectDB()
